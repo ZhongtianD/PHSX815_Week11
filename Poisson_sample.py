819d7326a42b8de0a94dbfd5b3ff4d3bb46ab1c1
Zhongtian Dong <78445813+ZhongtianD@users.noreply.github.com>
2021-04-22 20:48:31 -0500

    Update Poisson_sample.py

--- Poisson_sample.py
+++ Poisson_sample.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def main(lamb: float, Nsample: int, Nexperiment:int ):
-    Samples = np.random.poisson(lam=lamb, size=(Nexperiment,Nsample) )
+    Samples = np.random.exponential(scale=1/lamb, size=(Nexperiment,Nsample) )
     Mean = np.mean(Samples,axis=1)
     
     count, bins, ignored = plt.hist(Mean, 50, density=True)
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
 
     parser.add_argument("-L", "--lamb", type=float, default=2.0,
-                        help="lambda parameter of poisson distribution")
+                        help="lambda parameter of exponential distribution")
 
     parser.add_argument("-Ns", "--Nsample", type=int, default=10,
                         help="Number of samples for each experiment.")
