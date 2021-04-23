86459810708f85b9178e914ef8ca5900a7579442
Zhongtian Dong <78445813+ZhongtianD@users.noreply.github.com>
2021-04-22 20:37:34 -0500

    Update Poisson_sample.py

--- Poisson_sample.py
+++ Poisson_sample.py
@@ -4,12 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main(L: float, Ns: int, Ne:int ):
-    Samples = np.random.poisson(lam=L, size=(Ne,Ns) )
+def main(lamb: float, Nsample: int, Nexperiment:int ):
+    Samples = np.random.poisson(lam=lamb, size=(Nexperiment,Nsample) )
     Mean = np.mean(Samples,axis=1)
     
-    count, bins, ignored = plt.hist(G1, 20, density=True)
-    plt.title(str(Ne)+' experiments of '+str(Ns)+' samples')
+    count, bins, ignored = plt.hist(Mean, 20, density=True)
+    plt.title(str(Nexperiment)+' experiments of '+str(Nsample)+' samples')
     plt.show()
     plt.savefig('Poisson_mean.png')
     
@@ -19,7 +19,7 @@ def main(L: float, Ns: int, Ne:int ):
 if __name__ == '__main__':
     parser = ArgumentParser()
 
-    parser.add_argument("-L", "--lambda", type=float, default=2.0,
+    parser.add_argument("-L", "--lamb", type=float, default=2.0,
                         help="lambda parameter of poisson distribution")
 
     parser.add_argument("-Ns", "--Nsample", type=int, default=10,
